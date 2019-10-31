8817e3ca0c5cade10922d32a8d37f30f3f2e15eb
github username <github email address>
2019-10-30 21:57:29 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+	attr_accessor :name
+	attr_reader :roster
+
+	def initialize(name)
+		@name = name
+		@roster = {}
+	end
+
+	def add_student(student, grade)
+		#adds a new grade object to @roster if the grade does not already exist
+		@roster[grade] = [] if !@roster[grade]
+		#push student into specified grade array
+		@roster[grade] << student
+	end
+
+	def grade(lookup_grade)
+		@roster[lookup_grade]
+	end
+
+	def sort
+		#sort the hash by grade
+		sorted = Hash[@roster.sort]
+		#sort the hash by student
+		sorted.each { |grade, students| students.sort! }
+	end
+
+end
